d7d373ca7e10a8aeaf5b0319d955cc71440e1887
pap4skunk <madsengelberthj@gmail.com>
2026-04-27 14:29:51 +0200

    hejsa

---
+++ src/main/java/org/example/biludlejning/BiludlejningApplication.java
@@ -0,0 +1,13 @@
+package org.example.biludlejning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiludlejningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiludlejningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/biludlejning/BiludlejningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.biludlejning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiludlejningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
